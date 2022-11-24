a795eb1dc22456992bc523573b0a60392a02bdf2
Christopher K <christopher117@rocketmail.com>
2022-11-24 22:59:01 +0100

    Classification MLP

--- chris.py
+++ chris.py
@@ -1,7 +1,49 @@
-import inspect
+import numpy as np
+import chris_krimskrams
 
-def add( a,b = 6, **args ):
-  pass
+def ldData(data_dict, **args):
+    with open("./original_data/X_train.csv") as file:
+        list = file.readlines()[1:]
+    list = [np.array(x.split(","), dtype=np.float32) for x in list]
+    X_train = np.full((len(list),len(max(list,key = lambda x: len(x)))), np.nan, dtype=np.float32)
+    for i,j in enumerate(list):
+        X_train[i][0:len(j)] = j
+    data_dict["X_train"] = X_train[:,1:]
 
-print(str(inspect.signature(add)))
-print(add.__name__)
+    data_dict["y_train"] = np.loadtxt("./original_data/y_train.csv",
+                               dtype=np.intc,
+                               delimiter=',',
+                               skiprows=1)[:, 1:]
+
+    with open("./original_data/X_test.csv") as file:
+        list = file.readlines()[1:]
+    list = [np.array(x.split(","), dtype=np.float32) for x in list]
+    X_test = np.full((len(list),len(max(list,key = lambda x: len(x)))), np.nan, dtype=np.float32)
+    for i,j in enumerate(list):
+        X_test[i][0:len(j)] = j
+    data_dict["X_test"] = X_test[:,1:]
+
+    return data_dict
+
+def mlpClassification(data_dict, mlpClassification_epochs, mlpClassification_useValidationSet, mlpClassification_makePrediction, **args):
+  #data doesnt have to be normalized.
+
+  assert "X_train" in data_dict.keys()
+  assert "y_train" in data_dict.keys()
+  if mlpClassification_useValidationSet:
+    assert "X_val" in data_dict.keys()
+    assert "y_val" in data_dict.keys()
+  if mlpClassification_makePrediction:
+    assert "X_test" in data_dict.keys()
+
+
+  if mlpClassification_useValidationSet:
+    data_dict["train_losses"], data_dict["val_losses"], predict_funct = chris_krimskrams.train(mlpClassification_epochs, data_dict["X_train"], data_dict["y_train"], data_dict["X_val"], data_dict["y_val"], 32)
+  else:
+    data_dict["train_losses"], predict_funct = chris_krimskrams.train(mlpClassification_epochs, data_dict["X_train"], data_dict["y_train"], None, None, 32)
+
+  if mlpClassification_makePrediction:
+    data_dict["y_test"] = predict_funct(data_dict["X_test"])
+
+
+  return data_dict
---
+++ chris_testing.py
@@ -0,0 +1,28 @@
+import pipeline
+import numpy as np
+from pipeline import pipeline
+from chris import ldData, mlpClassification
+
+def custom(data_dict, **args):
+    x = data_dict["X_train"][:3000,:30]
+    y = data_dict["y_train"][:3000]
+    
+    data_dict["X_test"] = data_dict["X_test"][:,:30]
+
+    data_dict["X_val"] = data_dict["X_train"][3000:,:30]
+    data_dict["y_val"] = data_dict["y_train"][3000:]
+
+    data_dict["X_train"] = x
+    data_dict["y_train"] = y
+ 
+
+
+    return data_dict
+
+hyperparameter_dictionary = {"mlpClassification_epochs":20,
+                            "mlpClassification_useValidationSet":True, 
+                            "mlpClassification_makePrediction":True,
+                            }
+
+final_data_dict = pipeline([ldData, custom, mlpClassification],hyperparameter_dictionary,save_states_to_cache=False)
+print(final_data_dict)
