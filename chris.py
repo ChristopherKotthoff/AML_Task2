868562f2fa8d904b6ff5420e39d33b9860dbb340
Christopher K <christopher117@rocketmail.com>
2022-11-28 23:32:05 +0100

    finished first complete pipeline

--- chris.py
+++ chris.py
@@ -1,15 +1,17 @@
 import numpy as np
 import chris_krimskrams
 from biosppy.signals.ecg import christov_segmenter, extract_heartbeats
+from biosppy.signals.tools import normalize, synchronize
 from tqdm import tqdm
 from random import randint
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 def ldData(data_dict, **args):
     with open("./original_data/X_train.csv") as file:
         list = file.readlines()[1:]
     #list = [np.array(x.split(","), dtype=np.float32) for x in list]
-    list = [np.array(x.split(","), dtype=np.intc) for x in list]
+    list = [np.array(x.split(","), dtype=np.float32) for x in list]
     X_train = np.full((len(list),len(max(list,key = lambda x: len(x)))), np.nan, dtype=np.float32)
     for i,j in enumerate(list):
         X_train[i][0:len(j)] = j
@@ -23,7 +25,7 @@ def ldData(data_dict, **args):
     with open("./original_data/X_test.csv") as file:
         list = file.readlines()[1:]
     #list = [np.array(x.split(","), dtype=np.float32) for x in list]
-    list = [np.array(x.split(","), dtype=np.intc) for x in list]
+    list = [np.array(x.split(","), dtype=np.float32) for x in list]
     X_test = np.full((len(list),len(max(list,key = lambda x: len(x)))), np.nan, dtype=np.float32)
     for i,j in enumerate(list):
         X_test[i][0:len(j)] = j
@@ -67,7 +69,7 @@ def biosppyRPeaks(data_dict, **args):
       firstnan = np.where(np.isnan(ts))[0][0]
     except:
       firstnan = len(ts)
-    train_peaks.append(christov_segmenter(ts[:firstnan],sampling_rate=300))
+    train_peaks.append(christov_segmenter(ts[:firstnan],sampling_rate=300)["rpeaks"])
 
   test_peaks = []
 
@@ -76,7 +78,7 @@ def biosppyRPeaks(data_dict, **args):
       firstnan = np.where(np.isnan(ts))[0][0]
     except:
       firstnan = len(ts)
-    test_peaks.append(christov_segmenter(ts[:firstnan],sampling_rate=300))
+    test_peaks.append(christov_segmenter(ts[:firstnan],sampling_rate=300)["rpeaks"])
 
   data_dict["train_rpeaks"] = train_peaks
   data_dict["test_rpeaks"] = train_peaks
@@ -92,21 +94,21 @@ def biosppyExtract(data_dict, **args):
   
   heartbeat_templates_train = []
 
-  for ts in tqdm(data_dict["X_train"]):
+  for i, ts in tqdm(enumerate(data_dict["X_train"])):
     try:
       firstnan = np.where(np.isnan(ts))[0][0]
     except:
       firstnan = len(ts)
-    heartbeat_templates_train.append(extract_heartbeats(ts[:firstnan],data_dict["train_rpeaks"],sampling_rate=300)[0])
+    heartbeat_templates_train.append(extract_heartbeats(ts[:firstnan],data_dict["train_rpeaks"][i],sampling_rate=300)[0])
 
   heartbeat_templates_test = []
 
-  for ts in tqdm(data_dict["X_test"]):
+  for i, ts in tqdm(enumerate(data_dict["X_test"])):
     try:
       firstnan = np.where(np.isnan(ts))[0][0]
     except:
       firstnan = len(ts)
-    heartbeat_templates_test.append(extract_heartbeats(ts[:firstnan],data_dict["test_rpeaks"],sampling_rate=300)[0])
+    heartbeat_templates_test.append(extract_heartbeats(ts[:firstnan],data_dict["test_rpeaks"][i],sampling_rate=300)[0])
 
   data_dict["heartbeat_templates_train"] = heartbeat_templates_train
   data_dict["heartbeat_templates_test"] = heartbeat_templates_test
@@ -114,17 +116,126 @@ def biosppyExtract(data_dict, **args):
   return data_dict
 
 
-def plotNormHeartbeat(data_dict,index=None,**args):
+def plotNormHeartbeat(data_dict,plotNormHeartbeat_index=None,**args):
 
     assert "heartbeat_templates_train" in data_dict.keys()
 
-    if index==None:
-      index = randint(0,len(data_dict["heartbeat_templates_train"])-1)
+    if plotNormHeartbeat_index==None:
+      plotNormHeartbeat_index = randint(0,len(data_dict["heartbeat_templates_train"])-1)
 
-    mean = np.mean(data_dict["heartbeat_templates_train"][index], axis=0)
-    std = np.std(data_dict["heartbeat_templates_train"][index], axis=0)
+    mean = np.mean(data_dict["heartbeat_templates_train"][plotNormHeartbeat_index], axis=0)
+    std = np.std(data_dict["heartbeat_templates_train"][plotNormHeartbeat_index], axis=0)
 
     plt.plot(mean, color="green")
     plt.fill_between(np.arange(0,len(mean)),mean-std,mean+std, color="green", alpha=0.1)
 
+    plt.show()
+
     return data_dict
+
+
+def normalizeTemplates(data_dict,normalizeTemplates_normOverEntireTimeseries=False,**args):
+
+    assert "heartbeat_templates_train" in data_dict.keys()
+    assert "heartbeat_templates_test" in data_dict.keys()
+
+    heartbeat_templates_train= []
+    heartbeat_templates_test= []
+    if normalizeTemplates_normOverEntireTimeseries:
+      for l in data_dict["heartbeat_templates_train"]:
+        heartbeat_templates_train.append(normalize(l)["signal"])
+      for l in data_dict["heartbeat_templates_test"]:
+        heartbeat_templates_test.append(normalize(l)["signal"])
+    else:
+      for l in data_dict["heartbeat_templates_test"]:
+        heartbeat_templates_train.append([normalize(x)["signal"] for x in l])
+      for l in data_dict["heartbeat_templates_test"]:
+        heartbeat_templates_test.append([normalize(x)["signal"] for x in l])
+
+    data_dict["heartbeat_templates_train"] = heartbeat_templates_train
+    data_dict["heartbeat_templates_test"] = heartbeat_templates_test
+    
+
+    return data_dict
+
+
+def medmeanFeatures(data_dict, medmeanFeatures_useMedian=True,medmeanFeatures_useMedianSTD=True,**args):
+  assert "heartbeat_templates_train" in data_dict.keys()
+  assert "heartbeat_templates_test" in data_dict.keys()
+
+  mitte_features_train = []
+  mitte_features_test = []
+  for templates in data_dict["heartbeat_templates_train"]:
+    np_template = np.array(templates, dtype=np.float32)
+    if medmeanFeatures_useMedian:
+      mitte_features_train.append(np.median(np_template,axis=0))
+    else:
+      mitte_features_train.append(np.mean(np_template,axis=0))
+  for templates in data_dict["heartbeat_templates_test"]:
+    np_template = np.array(templates, dtype=np.float32)
+    if medmeanFeatures_useMedian:
+      mitte_features_test.append(np.median(np_template,axis=0))
+    else:
+      mitte_features_test.append(np.mean(np_template,axis=0))
+  
+  abweichung_features_train=[]
+  abweichung_features_test=[]
+  for i, templates in enumerate(data_dict["heartbeat_templates_train"]):
+    np_template = np.array(templates, dtype=np.float32)-mitte_features_train[i]
+    if medmeanFeatures_useMedianSTD:
+      abweichung_features_train.append(np.median(np.abs(np_template),axis=0))
+    else:
+      abweichung_features_train.append(np.sqrt(np.mean(np.square(np_template),axis=0)))
+  for i, templates in enumerate(data_dict["heartbeat_templates_test"]):
+    np_template = np.array(templates, dtype=np.float32)-mitte_features_test[i]
+    if medmeanFeatures_useMedianSTD:
+      abweichung_features_test.append(np.median(np.abs(np_template),axis=0))
+    else:
+      abweichung_features_test.append(np.sqrt(np.mean(np.square(np_template),axis=0)))
+
+  mitte_features_train = np.array(mitte_features_train)
+  mitte_features_test = np.array(mitte_features_test)
+
+  abweichung_features_train=np.array(abweichung_features_train)
+  abweichung_features_test=np.array(abweichung_features_test)
+
+
+  data_dict["X_train"] = np.append(mitte_features_train,abweichung_features_train,axis=1)
+  data_dict["X_test"] = np.append(mitte_features_test,abweichung_features_test,axis=1)
+
+  return data_dict
+
+
+def makeTrainValSet(data_dict,makeTrainValSet_valPercent=0.1, **args):
+  assert "X_train" in data_dict.keys()
+  assert "X_test" in data_dict.keys()
+  assert "y_train" in data_dict.keys()
+
+  data_dict["X_train"], data_dict["X_val"], data_dict["y_train"], data_dict["y_val"] = train_test_split(data_dict["X_train"],  data_dict["y_train"], test_size=makeTrainValSet_valPercent, random_state=42, shuffle=True)
+  
+  assert len(data_dict["y_train"].shape) == 2
+  assert len(data_dict["y_val"].shape) == 2
+
+  assert data_dict["y_train"].shape[1] == 1
+  assert data_dict["y_val"].shape[1] == 1
+
+  return data_dict
+
+'''def synchronizeTemplates(data_dict,**args):
+
+    assert "heartbeat_templates_train" in data_dict.keys()
+    assert "heartbeat_templates_test" in data_dict.keys()
+
+    heartbeat_templates_train= []
+    heartbeat_templates_test= []
+    for l in data_dict["heartbeat_templates_test"]:
+      heartbeat_templates_train.append([synchronize(l[0],x)["synch_y"] for x in l])
+    for l in data_dict["heartbeat_templates_test"]:
+      heartbeat_templates_test.append([synchronize(l[0],x)["synch_y"] for x in l])
+
+    data_dict["heartbeat_templates_train"] = heartbeat_templates_train
+    data_dict["heartbeat_templates_test"] = heartbeat_templates_test
+    
+
+    return data_dict
+'''
--- chris_testing.py
+++ chris_testing.py
@@ -1,31 +1,28 @@
 import pipeline
 import numpy as np
 from pipeline import pipeline
-from chris import ldData, mlpClassification, biosppyRPeaks
+from chris import ldData, mlpClassification, biosppyRPeaks, biosppyExtract, plotNormHeartbeat,normalizeTemplates,medmeanFeatures,makeTrainValSet
 from peak_detection_2_olin import peakDetecOlin
+from heinrich import crop, inv
 
-'''def custom(data_dict, **args):
-    x = data_dict["X_train"][:3000,:30]
-    y = data_dict["y_train"][:3000]
-    
-    data_dict["X_test"] = data_dict["X_test"][:,:30]
+def custom(data_dict, **args):
+    data_dict["X_train"] = data_dict["X_train"][:10]
+    data_dict["y_train"] = data_dict["y_train"][:10]
+    data_dict["X_test"] = data_dict["X_test"][:10]
 
-    data_dict["X_val"] = data_dict["X_train"][3000:,:30]
-    data_dict["y_val"] = data_dict["y_train"][3000:]
-
-    data_dict["X_train"] = x
-    data_dict["y_train"] = y
+    return data_dict
 
 
-
-    return data_dict'''
 
 
 hyperparameter_dictionary = {"mlpClassification_epochs":20,
                             "mlpClassification_useValidationSet":True, 
                             "mlpClassification_makePrediction":True,
+                            "plotNormHeartbeat_index":6,
+
+                            "normalizeTemplates_normOverEntireTimeseries":False,
                             }
 
 #final_data_dict = pipeline([ldData, custom, mlpClassification],hyperparameter_dictionary,save_states_to_cache=False)
-final_data_dict = pipeline([ldData, peakDetecOlin],hyperparameter_dictionary,save_states_to_cache=True)
-#print(final_data_dict)
+final_data_dict = pipeline([ldData, custom, crop, inv, peakDetecOlin, biosppyExtract, medmeanFeatures,makeTrainValSet,mlpClassification],hyperparameter_dictionary,save_states_to_cache=False)
+print(final_data_dict)
