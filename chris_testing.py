a5b36ebcaa63337f45b508e8569a7355210300e1
Christopher K <christopher117@rocketmail.com>
2022-11-30 13:53:01 +0100

    custom function prepares outlier input so that pipeline doesnt break

--- chris_testing.py
+++ chris_testing.py
@@ -7,9 +7,20 @@ from heinrich import crop, inv
 from olin_predictions import predictSimilarity
 
 def custom(data_dict, **args):
-    data_dict["X_train"] = data_dict["X_train"][885:888]
-    data_dict["y_train"] = data_dict["y_train"][885:888]
-    data_dict["X_test"] = data_dict["X_test"][:10]
+
+    try:
+      firstnan = np.where(np.isnan(data_dict["X_train"][886]))[0][0]
+    except:
+      firstnan = len(data_dict["X_train"][886])
+
+    usefulpart = data_dict["X_train"][886,2000:firstnan].copy()
+    data_dict["X_train"][886] = np.full(len(data_dict["X_train"][886]), np.nan, dtype=np.float32)
+    data_dict["X_train"][886,0:len(usefulpart)] = usefulpart
+    
+    #data_dict["X_train"] = data_dict["X_train"][885:888]
+    #data_dict["y_train"] = data_dict["y_train"][885:888]
+    #data_dict["X_test"] = data_dict["X_test"][:10]
+
 
     return data_dict
 
@@ -25,11 +36,6 @@ hyperparameter_dictionary = {"mlpClassification_epochs":20,
                             }
 
 #final_data_dict = pipeline([ldData, custom, mlpClassification],hyperparameter_dictionary,save_states_to_cache=False)
-<<<<<<< HEAD
-final_data_dict = pipeline([ldData, crop, inv, peakDetecOlin, biosppyExtract, medmeanFeatures,makeTrainValSet,predictSimilarity,savePred],hyperparameter_dictionary,save_states_to_cache=True)
-final_data_dict = pipeline([ldData, crop, inv, biosppyRPeaks, biosppyExtract, medmeanFeatures,makeTrainValSet,mlpClassification,savePred],hyperparameter_dictionary,save_states_to_cache=True)
+final_data_dict = pipeline([ldData, custom, crop, inv, peakDetecOlin, biosppyExtract, medmeanFeatures,makeTrainValSet,predictSimilarity,savePred],hyperparameter_dictionary,save_states_to_cache=True)
+#final_data_dict = pipeline([ldData, crop, inv, biosppyRPeaks, biosppyExtract, medmeanFeatures,makeTrainValSet,mlpClassification,savePred],hyperparameter_dictionary,save_states_to_cache=True)
 #print(final_data_dict)
-=======
-final_data_dict = pipeline([ldData, crop, inv, custom, biosppyRPeaks, biosppyExtract, medmeanFeatures, makeTrainValSet, mlpClassification],hyperparameter_dictionary,save_states_to_cache=False)
-print(final_data_dict)
->>>>>>> 3d0d16b11addca4b0c1fc49bfe1400bcfbbd7dd0
